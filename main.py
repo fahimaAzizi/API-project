4f1db901e1c84c75be6cda23c11bf94e0b7845c3
Fahima Azizi <fahimaazizi397@gmail.com>
2025-05-30 21:50:10 +0500

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api ,Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class Helloworld(Resource):
+    def get(self):
+        return{"data":"Hello world"}
+
+if __name__ == "__main__":
+    app.run(debug= True)
---
+++ test.py
@@ -0,0 +1,4 @@
+import requests
+
+BASE ="http://127.0.0.1:8000"
+requests = requests.get(BASE+"HELLLOWOLD")
